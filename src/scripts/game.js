44eb460d88c6ec8e7d72fa0ff8e1a8cbd3ed087a
root <somra45@gmail.com>
2023-06-29 23:53:16 -0400

    add draw castle feature

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -22,6 +22,7 @@ export default class CastleSiege {
         ctx.fillStyle = '#28b259'
         ctx.fillRect(0, 400, this.dimensions.width, 200)
         this.drawClouds(ctx)
+        this.drawCastle(ctx)
     };
 
     drawClouds(ctx, options = {}) {
@@ -35,18 +36,33 @@ export default class CastleSiege {
         let dy = 0
         let dWidth = 100
         let dHeight = 100
-        // need to get all of this into an animation function, and it could be moving clouds
-        ctx.drawImage(cloudImage, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+
+        // need to get all of this into an animation function, and it could 
+        //be moving clouds
+        ctx.drawImage(cloudImage, sx, sy, sWidth, sHeight, dx + 30, dy, 
+            dWidth, dHeight)
         ctx.drawImage(cloudImage, 500, 500, sWidth + 500, sHeight + 500,
         170, 100, 300, 300)
-        ctx.drawImage(cloudImage, sx, sy + 180, sWidth, sHeight, 280, 230, dWidth, dHeight -30)
+        ctx.drawImage(cloudImage, sx, sy + 180, sWidth, sHeight, 280, 230, 
+            dWidth, dHeight -30)
         ctx.drawImage(cloudImage, 1000, 500, sWidth + 600, sHeight + 500,
             0, 110, 200, 200)
-        ctx.drawImage(cloudImage, 500, 650, sWidth + 800, sHeight,
+        ctx.drawImage(cloudImage, 500, 650, sWidth + 680, sHeight,
             370, 10, 300, 100)
-        ctx.drawImage(cloudImage, 350, 650, sWidth + 590, sHeight,
+        ctx.drawImage(cloudImage, 465, 650, sWidth + 590, sHeight,
             225, 78, 300, 80)
-        // ctx.drawImage(cloudImage, 500, 650, sWidth + 800, sHeight,
-        //     380, 110, 300, 100)
-    };  
+            ctx.drawImage(cloudImage, sx, sy + 150, sWidth, sHeight, dx + 150, 
+                dy, dWidth, dHeight)
+            ctx.drawImage(cloudImage, sx, sy + 60, sWidth, sHeight, dx + 57, 
+                dy + 290, dWidth, dHeight)
+        };  
+
+    drawCastle(ctx, options = {}) {
+        const castleImage = document.getElementById('castle-image')
+        if (options !== {}) {
+            ctx.drawImage(castleImage, options.dx, options.dy, options.dWidth, 
+                options.dHeight)
+        }
+        ctx.drawImage(castleImage, 480, 274, 200, 200)
+    }
 };
